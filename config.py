cb489fc44f11d36352b64fc241d1a918231eeee4
Nurfiz <128210420+Nurfiz@users.noreply.github.com>
2023-06-09 22:30:29 +0300

    test

---
+++ config.py
@@ -0,0 +1,10 @@
+questions = [
+     {'text': 'Любите ли вы животных ?', 'type': 'closed', 'options': ['Да', 'Нет'],
+     'answer': 'Да'},
+    {'text': 'Часто ли вы гуляете ?',
+     'type': 'closed', 'options': ['Да', 'Нет'], 'answer': 'Да'},
+    {'text': 'Любите ли находиться в больших компаниях ?', 'type':'closed', 'options': ['Да', 'Нет'], 'answer': 'Да'},
+    {'text': 'Занимаетесь ли вы спортом ?', 'type': 'closed', 'options': ['Да', 'Нет'], 'answer': 'Да'},
+    {'text': 'Любите ли вы читать ?', 'type': 'closed','options': ['Да', 'Нет'], 'answer': 'Да'},
+    {'text': 'Любите ли вы больших собак?', 'type': 'closed', 'options': ['Да ', 'Нет', ], 'answer': 'Да'}
+]
